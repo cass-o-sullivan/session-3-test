7463414cb17123c3352acce4de528373815eaf61
cass-o-sullivan <cassreeceosullivan@gmail.com>
2025-08-07 09:20:18 +0800

    edited python file

--- python_test.py
+++ python_test.py
@@ -1,1 +1,2 @@
-print("When you're scared, that's all the more reason to move forward!")
+print("When you're scared, that's all the more reason to move forward!\n")
+print("The dreams of those who've fallen! The hopes of those who'll follow! Those two sets of dreams weave together into a double helix! Drilling a path towards tomorrow! And that's Tengen Toppa! That's Gurren Lagann!\n")
